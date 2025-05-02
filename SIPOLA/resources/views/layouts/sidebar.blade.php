2a0a8c6eda6769715747859b7e82ddce796e47dd
RizkyaSalsabila <rzkysalsabila@gmail.com>
2025-05-02 22:54:07 +0700

    Merubah tampilan sidebar sementara sesuai user

---
+++ SIPOLA/resources/views/layouts/sidebar.blade.php
@@ -0,0 +1,47 @@
+        <!-- Menu -->
+          <div class="menu-divider mt-0"></div>
+
+          <div class="menu-inner-shadow"></div>
+
+          <ul class="menu-inner py-1">
+            <!-- Dashboards -->
+            <li class="menu-item active open">
+              <a href="javascript:void(0);" class="menu-link">
+                <i class="menu-icon tf-icons bx bx-home"></i>
+                <div class="text-truncate" data-i18n="Dashboards">Beranda</div>
+              </a>
+            </li>
+
+            <!-- Layouts -->
+            <li class="menu-item">
+              <a href="javascript:void(0);" class="menu-link">
+                <i class="menu-icon tf-icons bx bx-user"></i>
+                <div class="text-truncate" data-i18n="Layouts">Profil</div>
+              </a>
+            </li>
+
+            <!-- Pages -->
+            <li class="menu-item">
+              <a href="javascript:void(0);" class="menu-link">
+                <i class="menu-icon tf-icons bx bx-medal"></i>
+                <div class="text-truncate" data-i18n="Account Settings">Tambah Prestasi</div>
+              </a>
+            </li>
+
+            <li class="menu-item">
+              <a href="javascript:void(0);" class="menu-link">
+                <i class="menu-icon tf-icons bx bx-trophy"></i>
+                <div class="text-truncate" data-i18n="Authentications">Manajemen Lomba</div>
+              </a>
+            </li>
+
+            <div style="flex-grow: 1;"></div>
+
+            <li class="menu-item">
+              <a href="javascript:void(0);" class="menu-link">
+                <i class="menu-icon tf-icons bx bx-log-out"></i>
+                <div class="text-truncate" data-i18n="Misc">Logout</div>
+              </a>
+            </li>
+          </ul>
+        <!-- / Menu -->
