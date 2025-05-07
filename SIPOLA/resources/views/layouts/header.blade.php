54b8326fbfe9e2f46fd6e49e852290d87eb59caf
RizkyaSalsabila <rzkysalsabila@gmail.com>
2025-05-07 14:46:10 +0700

    Menambahkan halaman manage user bagi admin secara frontend
    dan mengaktifkan sidebar yang berkaitan

--- SIPOLA/resources/views/layouts/header.blade.php
+++ SIPOLA/resources/views/layouts/header.blade.php
@@ -30,7 +30,7 @@ id="layout-navbar">
         href="javascript:void(0);"
         data-bs-toggle="dropdown">
         <div class="avatar avatar-online">
-          <img src="../assets/img/avatars/1.png" alt class="w-px-40 h-auto rounded-circle" />
+          <img src="{{ asset('template/assets/img/avatars/1.png') }}" alt class="w-px-40 h-auto rounded-circle" />
         </div>
       </a>
       <ul class="dropdown-menu dropdown-menu-end">
@@ -43,7 +43,7 @@ id="layout-navbar">
                 </div>
               </div>
               <div class="flex-grow-1">
-                <h6 class="mb-0">John Doe</h6>
+                <h6 class="mb-0">Tyara Kusuma Nevada</h6>
                 <small class="text-body-secondary">Admin</small>
               </div>
             </div>
@@ -55,20 +55,6 @@ id="layout-navbar">
         <li>
           <a class="dropdown-item" href="#">
             <i class="icon-base bx bx-user icon-md me-3"></i><span>My Profile</span>
-          </a>
-        </li>
-        <li>
-          <a class="dropdown-item" href="#">
-            <i class="icon-base bx bx-cog icon-md me-3"></i><span>Settings</span>
-          </a>
-        </li>
-        <li>
-          <a class="dropdown-item" href="#">
-            <span class="d-flex align-items-center align-middle">
-              <i class="flex-shrink-0 icon-base bx bx-credit-card icon-md me-3"></i
-              ><span class="flex-grow-1 align-middle">Billing Plan</span>
-              <span class="flex-shrink-0 badge rounded-pill bg-danger">4</span>
-            </span>
           </a>
         </li>
         <li>
