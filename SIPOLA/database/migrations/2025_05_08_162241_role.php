4d7af57fc94d3470186140c5b329e34f2282f0a1
RizkyaSalsabila <rzkysalsabila@gmail.com>
2025-05-09 00:14:28 +0700

    Memperbaiki error migrasi dan seed

--- SIPOLA/database/migrations/2025_05_08_162241_role.php
+++ SIPOLA/database/migrations/2025_05_08_162241_role.php
@@ -1,11 +1,15 @@
 <?php
+
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class role extends Migration
+return new class extends Migration
 {
-    public function up()
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
     {
         Schema::create('role', function (Blueprint $table) {
             $table->id('id_role');
@@ -15,8 +19,11 @@ class role extends Migration
         });
     }
 
-    public function down()
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
     {
         Schema::dropIfExists('role');
     }
-}
+};
