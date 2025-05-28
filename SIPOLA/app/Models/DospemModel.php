ea559e39c086f6fcc0ca5734269da8bbff8abff9
RizkyaSalsabila <rzkysalsabila@gmail.com>
2025-05-29 03:11:25 +0700

    Menambahkan fitur verifikasi prestasi mahasiswa bagi admin, menambahkan catatan, dan pengelolaan filter berdasarkan status

--- SIPOLA/app/Models/DospemModel.php
+++ SIPOLA/app/Models/DospemModel.php
@@ -19,11 +19,5 @@ class DospemModel extends Model
     public function rekomendasi() {
         return $this->hasMany(RekomendasiModel::class, 'id_dosen');
     }
-
-    // app/Models/DospemModel.php
-    public function role()
-        {
-            return $this->belongsTo(RoleModel::class, 'id_role');
-        }
-
+    
 }
