b9ac73ac89d906b5aee3039c18ad3fcca59b483c
RizkyaSalsabila <rzkysalsabila@gmail.com>
2025-06-02 21:15:15 +0700

    Menambahkan relasi antara tabel role dengan users untuk membedakan user yang input lomba

--- SIPOLA/app/Models/User.php
+++ SIPOLA/app/Models/User.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -11,7 +12,7 @@ class User extends Authenticatable
     use Notifiable;
 
     protected $fillable = [
-        'name', 'username', 'email', 'password', 'role',
+        'name', 'username', 'email', 'password', 'id_role',
     ];
 
     protected $hidden = [
@@ -20,5 +21,9 @@ class User extends Authenticatable
 
     public function mahasiswa(): HasOne {         //1 user memiliki 1 mahasiswa
         return $this->hasOne(MahasiswaModel::class, 'user_id');
+    }
+
+    public function role(): BelongsTo {         
+        return $this->belongsTo(RoleModel::class, 'id_role', 'id_role');
     }
 }
